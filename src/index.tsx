2b427f66de367eacec1689f77569a78624c67d10
Marc Schaerer <marc@dreamora.ch>
2021-05-26 22:53:53 +0200

    ADDED: Dynamic list experiment with local state
    Experiment get a clearer understanding of typescript specifics in the context of React state management and typesafe checs.

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import GuestList from './state/GuestList';
+
+const App = () => {
+    return <div>
+        <GuestList/>
+    </div>
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
