1f8e30c022c17bd465c6e5b13b5260e4df75e5f1
Marc Schaerer <marc@dreamora.ch>
2021-05-26 23:46:43 +0200

    ADDED: Class based component UserSearch
    Adds a class based React Component implementation version of the UserSearch to experiment with the benefits and drawbacks in practice.
    
    In particular the ability to provide the data set to the generic is a powerful aspect as the typesafety aspect ensures that any data received from any endpoint will be provided in the appropriate structure.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom';
 // import GuestList from './state/GuestList';
 import EventComponent from './events/EventComponent';
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
+import UserSearch from './classes/UserSearch';
 
 const App = () => {
     return <div>
         <EventComponent />
-        <UserSearch />
+        <UserSearch users={[
+            {name: 'Sarah', age: 20},
+            {name: 'Alex', age: 20},
+            {name: 'Michael', age: 20}
+        ]} />
     </div>
 };
 
