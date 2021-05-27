fd881331b8b356f6513d36329ac5cacc918e2250
Marc Schaerer <marc@dreamora.ch>
2021-05-27 22:09:52 +0200

    ADDED: working with typesafe React refs in typescript
    This is a little experiment with the UserSearch with refs to see the implications of typescript on this and how the interfaces look like and need to be declared.

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,13 @@ import ReactDOM from 'react-dom';
 // import GuestList from './state/GuestList';
 import EventComponent from './events/EventComponent';
 // import UserSearch from './state/UserSearch';
-import UserSearch from './classes/UserSearch';
+// import UserSearch from './classes/UserSearch';
+import UserSearch from './refs/UserSearch';
 
 const App = () => {
     return <div>
         <EventComponent />
-        <UserSearch users={[
-            {name: 'Sarah', age: 20},
-            {name: 'Alex', age: 20},
-            {name: 'Michael', age: 20}
-        ]} />
+        <UserSearch />
     </div>
 };
 
