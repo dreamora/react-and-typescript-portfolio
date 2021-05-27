fcfaf918a3030b689fc0b52d5dfb577f2b3b47f5
Marc Schaerer <marc@dreamora.ch>
2021-05-27 23:38:30 +0200

    ADDED: NPM package search action creator
    Implementation of a typesafe action creator with Redux Dispatch that supports clean, compiletime checked search request dispatching.

---
+++ redux-ts/src/state/action-creators/index.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import {Dispatch} from "redux";
+import {ActionType} from "../action-types";
+import {Action} from '../actions';
+
+export const searchPackages = (term: string) => {
+    return async (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.SEARCH_PACKAGES
+        });
+
+        try {
+            const {data} = await axios.get('https://registry.npmjs.org/-/v1/search',
+                {
+                    params: {
+                        text: term
+                    }
+                });
+
+            const names = data.objects.map((result: any) => {
+                return result.package.name
+            });
+
+            dispatch({
+                type   : ActionType.SEARCH_PACKAGES_SUCCESS,
+                payload: names
+            });
+        } catch (err) {
+            dispatch({
+                type   : ActionType.SEARCH_PACKAGES_ERROR,
+                payload: err.message
+            })
+        }
+    };
+};
